e8e8bc88572746b343ada2f24f9b981ad756f2bc
tayson13 <ab.naser01@gmail.com>
2023-06-20 12:45:54 +0600

    35 remodified

--- prac.js
+++ prac.js
@@ -4,14 +4,23 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-    const length = nums.length
-    for(let i=0;i<length;i++){
+    
+    let start = 0
+    let mid = 0
+    let end = nums.length-1
 
-        if(nums[i]>target ||nums[i]==target){
-            return i
-        }
+    while(start<=end){
+        
+        mid = Math.floor((start+end)/2)
+
+        console.log (start,mid,end)
+
+        if(nums[mid] == target)
+            return mid
+
+        nums[mid] > target? end = mid-1: start = mid+1
     }
-    return length
+    return end+1
 };
 
-console.log(searchInsert([5,6,7,8],9))
+console.log(searchInsert([1,3,6,7,9],4))
